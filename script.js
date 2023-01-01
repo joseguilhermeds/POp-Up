89fbf68756e6b170d3e7534b274eebb17bb9af4e
José Guilherme Müller da Silva <100322648+joseguilhermeds@users.noreply.github.com>
2022-12-31 22:42:39 -0300

    Página pop-up

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector(' .modal-containers ')
+
+function openmodal(){
+    modal.classList.add('active');
+}
+
+function closemodal() {
+    modal.classList.remove('active');
+}
